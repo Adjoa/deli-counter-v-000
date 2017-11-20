b1cf317b67a24456d653c43490eff4921923517f
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-20 15:26:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,13 @@
 katz_deli = ["Logan", "Avi", "Spencer"]
 
-def display_current_line(katz_deli)
-  number = 0
-  katz_deli.each do |customer|
-    print "#{number + 1}. #{katz_deli[number]} "
-    number+=1
-  end
-end
-
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
     print "The line is currently: "
-    print "#{display_current_line(katz_deli)}"
+    katz_deli.each_with_index do |customer, number|
+      print "#{number + 1}. #{katz_deli[number]} "
+    end
   end
 end
 
