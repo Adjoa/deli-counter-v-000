81d436f230b389f87c3e9afcbf0d4d57bc001b04
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-20 15:14:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,11 @@
+def print_current_line(katz_deli)
+  number = 0
+  katz_deli.each do |customer|
+    puts "#{number + 1}. #{katz_deli[number]}"
+    number+=1 
+  end
+end
+
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
