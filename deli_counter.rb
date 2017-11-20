04bc1b3d53b531a8c2753ab421271860c3730a12
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-20 15:42:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,10 +12,12 @@ def line(katz_deli)
   end
 end
 
+
 def take_a_number(katz_deli, new_customer)
   katz_deli.push(new_customer)
   puts "Welcome, #{new_customer}. You are number #{katz_deli.size} in line."
 end
+
 
 def now_serving(katz_deli)
   if katz_deli == []
