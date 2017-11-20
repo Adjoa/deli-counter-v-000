5f167e5263003b3c79cbd928f8658184c2417234
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-20 15:42:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,7 @@ def now_serving(katz_deli)
   if katz_deli == []
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.unshift}."
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
   end
 end
