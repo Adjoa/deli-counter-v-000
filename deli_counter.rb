3e310ec3310a7f003a91ad63697fdf74f5c5df0e
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-20 15:33:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
       list.push "#{index + 1}. #{katz_deli[index]} "
     end
     # puts list
-    ["The line is currently: ", list].join.strip
+    return ["The line is currently: ", list].join.strip
   end
 end
 
