5154ca22c075cac7dfaba987d943c7e9053f2cd9
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-20 15:14:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def print_current_line(katz_deli)
   number = 0
   katz_deli.each do |customer|
     puts "#{number + 1}. #{katz_deli[number]}"
-    number+=1 
+    number+=1
   end
 end
 
@@ -10,6 +10,6 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently:"
+    puts "The line is currently: #{print_current_line(katz_deli)}"
   end
 end
