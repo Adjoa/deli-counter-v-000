6246703efcd400b6f0b7e7b772ec332466bb1997
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-20 15:36:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,17 @@
-# katz_deli = ["Logan", "Avi", "Spencer"]
-
 def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
     list = []
+
     katz_deli.each_with_index do |customer, index|
       list.push "#{index + 1}. #{katz_deli[index]} "
     end
-    # puts list
+
     puts ["The line is currently: ", list].join.strip
   end
 end
 
-# line(katz_deli)
+def take_a_number(katz_deli, new_customer)
+  puts "Welcome, #{new_customer}. You are number #{katz_deli.size + 1} in line."
+end
